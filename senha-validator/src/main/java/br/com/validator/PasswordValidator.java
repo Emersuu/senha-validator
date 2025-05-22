0c4ed7cfaa4bd4620bbbd033d77bd54776dcafba
Felipe dos Santos Ferreira <felipesasntosfs2409@gmail.com>
2025-05-22 15:17:26 -0300

    feat: implementar validação de senha com no mínimo 8 caracteres

--- senha-validator/src/main/java/br/com/validator/PasswordValidator.java
+++ senha-validator/src/main/java/br/com/validator/PasswordValidator.java
@@ -1,31 +1,14 @@
-
 package br.com.validator;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PasswordValidator {
-
     public ValidationResult validate(String senha) {
         List<String> errors = new ArrayList<>();
 
         if (senha.length() < 8) {
             errors.add("A senha deve ter pelo menos 8 caracteres");
-        }
-
-        long digitCount = senha.chars().filter(Character::isDigit).count();
-        if (digitCount < 2) {
-            errors.add("A senha deve conter pelo menos 2 dígitos");
-        }
-
-        boolean hasUppercase = senha.chars().anyMatch(Character::isUpperCase);
-        if (!hasUppercase) {
-            errors.add("A senha deve conter pelo menos uma letra maiuscula");
-        }
-
-        boolean hasSpecialChar = senha.chars().anyMatch(c -> "!@#$%^&*()-_=+[{]}|;:'",<.>/?`~".indexOf(c) >= 0);
-        if (!hasSpecialChar) {
-            errors.add("A senha deve conter pelo menos um caractere especial");
         }
 
         return new ValidationResult(errors.isEmpty(), errors);
